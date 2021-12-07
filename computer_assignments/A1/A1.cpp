67a5782df30cad67e51f1f28b17098ea56974b70
Mehrab Farhadzadeh <mehrab7fm@gmail.com>
2021-12-07 15:17:54 +0330

    Try to implement "simple_encryption" function

--- computer_assignments/A1/A1.cpp
+++ computer_assignments/A1/A1.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 using namespace std;
 
 void get_request_info(string &mode, string &approach, string &key);
 void get_files_paths(string &inFilePath, string &outFilePath);
+char get_char_from_file(ifstream inFile);
+char put_char_into_file(ofstream outFile, char ch);
 
 void encryption(string approach, string key);
 void simple_encryption(string key);
+int encrypt_char_simple(char ch);
 void complicated_encryption(string key);
 
 void decryption(string approach, string key);
 void simple_decryption(string key);
 void complicated_decryption(string key);
+
 
 int main()
 {
@@ -55,7 +60,17 @@ void decryption(string approach, string key)
 
 void simple_encryption(string key)
 {
+  string inFilePath, outFilePath;
+  get_files_paths(inFilePath, outFilePath);
 
+  ofstream outFile;
+  ifstream inFile;
+  inFile.open(inFilePath);
+  outFile.open(outFilePath);
+
+  char ch = get_char_from_file(inFile);
+  int ecncrypted_ch = encrypt_char_simple(ch);
+  put_char_into_file(outFile, (char)ecncrypted_ch);
 }
 void complicated_encryption(string key)
 {
