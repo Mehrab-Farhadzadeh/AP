0acde76184978cea7ecf17e99725c2973c28f470
Mehrab Farhadzadeh <mehrab7fm@gmail.com>
2021-12-10 19:32:42 +0330

    Implement "main()"

--- computer_assignments/APS98-A0/APS98-A0_02.cpp
+++ computer_assignments/APS98-A0/APS98-A0_02.cpp
@@ -8,7 +8,17 @@ void print_result(bool result);
 
 int main()
 {
+  string num_str;
+  cin >> num_str;
 
+  int counter = 1;
+  while (num_str != "0")
+  {
+    cout << "Case " << counter << ": ";
+    print_result(is_charkheshi(num_str));
+    cin >> num_str;
+    counter++;
+  }
   return 0;
 }
 
