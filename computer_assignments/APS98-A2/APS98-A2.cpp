1b06f1938f84830c98b357bcaf81279d4e4677b2
Mehrab Farhadzadeh <mehrab7fm@gmail.com>
2021-12-16 23:04:31 +0330

    First attempt; Add pdf and txt files

--- computer_assignments/APS98-A2/APS98-A2.cpp
+++ computer_assignments/APS98-A2/APS98-A2.cpp
@@ -11,7 +11,8 @@ int main()
 {
   vector<string> queues = get_queues();
 
-  for (int queue = 0; queue < queues.size(); queue++)
+  int queues_size = (int)queues.size();
+  for (int queue = 0; queue < queues_size; queue++)
   {
     validate_and_print_queue(queues[queue]);
   }
@@ -21,12 +22,29 @@ int main()
 
 vector<string> get_queues(void)
 {
+  vector<string> queues;
+  string queue;
+  while (cin >> queue)
+    queues.push_back(queue);
 
+  return queues;
 }
 
 void validate_and_print_queue(string queue, int curr_pos)
 {
+  if (curr_pos >= (int)queue.size() - 1)
+  {
+    cout << endl;
+    return;
+  }
+  if (queue[curr_pos] == queue[curr_pos + 1])
+  {
+    validate_and_print_queue(queue, curr_pos + 1);
+    return;
+  }
 
+  cout << queue[curr_pos];
+  validate_and_print_queue(queue, curr_pos + 1);
 }
 
 void validate_and_print_queue(string queue)
