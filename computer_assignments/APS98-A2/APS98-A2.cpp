47ac08bf2770451eb380ace73804a5fb099d14dd
Mehrab Farhadzadeh <mehrab7fm@gmail.com>
2021-12-16 23:39:42 +0330

    Final version

--- computer_assignments/APS98-A2/APS98-A2.cpp
+++ computer_assignments/APS98-A2/APS98-A2.cpp
@@ -32,8 +32,15 @@ vector<string> get_queues(void)
 
 void validate_and_print_queue(string queue, int curr_pos)
 {
-  if (curr_pos >= (int)queue.size() - 1)
+  if (curr_pos >= (int)queue.size() - 2)
   {
+    if (queue[curr_pos] == queue[curr_pos + 1])
+      cout << queue[curr_pos];
+      else
+      {
+        cout << queue[curr_pos];
+        cout << queue[curr_pos + 1];
+      }
     cout << endl;
     return;
   }
