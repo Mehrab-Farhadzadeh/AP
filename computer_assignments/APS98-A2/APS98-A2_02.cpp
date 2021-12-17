b8ce2fc3fafe720a47275e1978c74e318d136535
Mehrab Farhadzadeh <mehrab7fm@gmail.com>
2021-12-18 01:23:48 +0330

    "sentences()", and "is_meaningful()"

--- computer_assignments/APS98-A2/APS98-A2_02.cpp
+++ computer_assignments/APS98-A2/APS98-A2_02.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 using namespace std;
 
-vector<vector<int>> words_locations;
+vector<vector<int>> words_locations_on_str;
+
 vector<string> get_spaceless_strs(void);
 void print_all_sentences(string spaceless_str);
 bool sentences(string spaceless_strs, int after_this_location);
+bool is_meaningful(string spaceless_strs, int after_this_location, int end_of_word);
 
 int main()
 {
@@ -34,6 +37,46 @@ vector<string> get_spaceless_strs(void)
 
 void print_all_sentences(string spaceless_str)
 {
-  
+  sentences(spaceless_str, 0);
 }
 
+bool sentences(string spaceless_strs, int after_this_location)
+{
+  if (after_this_location >= (int)spaceless_strs.size())
+  {
+    return true;
+  }
+
+  for (int end_of_word = after_this_location + 1; end_of_word <= (int)spaceless_strs.size(); end_of_word++)
+  {
+    if (is_meaningful(spaceless_strs, after_this_location, end_of_word))
+    {
+      vector<int> loacations;
+      loacations.push_back(after_this_location);
+      loacations.push_back(end_of_word);
+      words_locations_on_str.push_back(loacations);
+
+      if (sentences(spaceless_strs, end_of_word))
+        return true;
+      words_locations_on_str.pop_back();
+    }
+  }
+  return false;
+}
+
+bool is_meaningful(string spaceless_strs, int after_this_location, int end_of_word)
+{
+  int len = end_of_word - after_this_location;
+  string word = spaceless_strs.substr(after_this_location, len);
+
+  ifstream meaningful_words_file("words.txt");
+
+  string readed_word;
+  while (meaningful_words_file >> readed_word)
+    if (readed_word == word)
+      return true;
+      
+  meaningful_words_file.close();
+
+  return false;
+}
