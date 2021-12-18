e7ce42bb778c085b38d83b0f5919b8e786e61821
Mehrab Farhadzadeh <mehrab7fm@gmail.com>
2021-12-18 21:30:38 +0330

    Initial design

--- computer_assignments/APS98-A2/APS98-A2_03.cpp
+++ computer_assignments/APS98-A2/APS98-A2_03.cpp
@@ -1,11 +1,44 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <fstream>
 using namespace std;
+
+vector<Movement> moves;
+
+struct Movement
+{
+  int row;
+  int col;
+  char dir;
+};
+
+typedef vector<string> map_t;
+
+map_t get_map();
+
+bool play(map_t map, Movement movement);
+void print_moves();
+void print_error();
 
 int main()
 {
+  map_t map = get_map();
+  Movement movement;
+
+  if (play(map, movement))
+    print_moves();
+  else
+    print_error();
 
   return 0;
+}
+
+map_t get_map()
+{
+  map_t map;
+  string line;
+  while (cin >> line)
+    map.push_back(line);
+
+  return map;
 }
