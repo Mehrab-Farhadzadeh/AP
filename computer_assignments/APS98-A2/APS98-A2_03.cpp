a126b0acff9dbeb115fbbcb9d987aad3a6272a10
Mehrab Farhadzadeh <mehrab7fm@gmail.com>
2021-12-18 21:54:21 +0330

    Complete init design

--- computer_assignments/APS98-A2/APS98-A2_03.cpp
+++ computer_assignments/APS98-A2/APS98-A2_03.cpp
@@ -20,6 +20,12 @@ bool play(map_t map, Movement movement);
 void print_moves();
 void print_error();
 
+bool find_movement(string line, Movement movement);
+void apply_movement(map_t map, Movement movement);
+void discard_movement(map_t map, Movement movement);
+bool is_there_move(map_t map);
+int lefted_heros(map_t map);
+
 int main()
 {
   map_t map = get_map();
@@ -54,5 +60,22 @@ void print_error()
 
 bool play(map_t map, Movement movement)
 {
+  for (int line = 0; line < (int)map.size(); line++)
+  {
+    while(find_movement(map[line], movement))
+    {
+      apply_movement(map, movement);
+      if(play(map, movement))
+        return true;
+      discard_movement(map, movement);
+    }
+  }
 
+  if (is_there_move(map))
+    play(map, movement);
+
+  if (lefted_heros(map) == 1)
+    return true;
+
+  return false;
 }
