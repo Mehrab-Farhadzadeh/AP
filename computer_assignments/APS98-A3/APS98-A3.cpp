ef922a58ed2d5f3b4d8b65fc1b4bd85611fbc2fb
Mehrab Farhadzadeh <mehrab7fm@gmail.com>
2021-12-25 20:01:00 +0330

    Try to implement "read_and_store()"

--- computer_assignments/APS98-A3/APS98-A3.cpp
+++ computer_assignments/APS98-A3/APS98-A3.cpp
@@ -5,13 +5,13 @@ using namespace std;
 
 enum weekday
 {
+    Saturday,
     Sunday,
     Monday,
     Tuesday,
     Wednesday,
     Thursday,
-    Friday,
-    Saturday
+    Friday
 };
 
 enum hour
@@ -49,18 +49,22 @@ struct Session
     Time start;
     Time end;
     weekday day;
-    course_t course;
+    course_id_t id;
     group_t group;
 };
 
 typedef int group_t;
-typedef string course_t;
+typedef string course_id_t;
 typedef vector<Session> timeline_t;
 typedef vector<timeline_t> day_t;
 typedef vector<day_t> week_t;
 
 void read_and_store(week_t week);
 void visualize(week_t week);
+
+void add_day_to_session(Session session, string weekday_name);
+void read_and_add_date_to_session(Session session);
+void add_session_to_timeline(day_t day,Session session);
 
 int main()
 {
@@ -73,8 +77,21 @@ int main()
 
 void read_and_store(week_t week)
 {
-    
+    Session session;
+    char delimiter;
+    cin >> session.id >> delimiter >> session.group;
+    string weekday_name;
+    while (cin >> weekday_name)
+    {
+        add_day_to_session(session, weekday_name);
+        read_and_add_date_to_session(session);
+        add_session_to_timeline(week[session.day], session);
+    }
 }
+
+void add_day_to_session(Session session, string weekday_name);
+void read_and_add_date_to_session(Session session);
+void add_session_to_timeline(day_t day, Session session);
 
 void visualize(week_t week)
 {
