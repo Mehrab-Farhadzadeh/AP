6ed0040eb1c1b84ccc612939fda3a468cbc2eae7
Mehrab Farhadzadeh <mehrab7fm@gmail.com>
2022-02-11 22:02:11 +0330

    Init Interface.cpp

---
+++ computer_assignments/APS98-A4/Interface.hpp
@@ -0,0 +1,31 @@
+#ifndef INTERFACE_HPP
+#define INTERFACE_HPP
+
+#include "RoundRobinScheduler.hpp"
+
+#define RESET "\033[0m"
+#define BLACK "\033[30m"              /* Black */
+#define RED "\033[31m"                /* Red */
+#define GREEN "\033[32m"              /* Green */
+#define YELLOW "\033[33m"             /* Yellow */
+#define BLUE "\033[34m"               /* Blue */
+#define MAGENTA "\033[35m"            /* Magenta */
+#define CYAN "\033[36m"               /* Cyan */
+#define WHITE "\033[37m"              /* White */
+#define BOLDBLACK "\033[1m\033[30m"   /* Bold Black */
+#define BOLDRED "\033[1m\033[31m"     /* Bold Red */
+#define BOLDGREEN "\033[1m\033[32m"   /* Bold Green */
+#define BOLDYELLOW "\033[1m\033[33m"  /* Bold Yellow */
+#define BOLDBLUE "\033[1m\033[34m"    /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m" /* Bold Magenta */
+#define BOLDCYAN "\033[1m\033[36m"    /* Bold Cyan */
+#define BOLDWHITE "\033[1m\033[37m"   /* Bold White */
+
+// 1. add_core
+// 2. add_process <number_of_process_threads> <number_of_time_slices_per_first_thread> ...
+// 3. show_cores_stat
+// 4. run_cores
+// 5. finish_tasks
+void interface(RoundRobinScheduler &round_robin_scheduler);
+
+#endif
