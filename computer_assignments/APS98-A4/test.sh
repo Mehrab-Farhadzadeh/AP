6e0e32507d634f85cd8f78cc445e49d7d91a5199
Mehrab Farhadzadeh <mehrab7fm@gmail.com>
2022-02-13 01:27:46 +0330

    remove duplicated testcase

--- computer_assignments/APS98-A4/test.sh
+++ computer_assignments/APS98-A4/test.sh
@@ -5,6 +5,3 @@ do
     make run < testcases/Testcases/input/$i.in | colordiff testcases/Testcases/output/$i.out -
     let i++
 done
-
-echo $i
-    make run < testcases/sample\ testcases/sample.in | colordiff testcases/sample\ testcases/sample.out -
