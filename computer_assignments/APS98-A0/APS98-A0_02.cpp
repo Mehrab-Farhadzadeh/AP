7451e29afca5502f9222124301e7022397fea1fb
Mehrab Farhadzadeh <mehrab7fm@gmail.com>
2022-02-23 01:32:38 +0330

    Refactoring: Replace "while" loop with "for" loop

--- computer_assignments/APS98-A0/APS98-A0_02.cpp
+++ computer_assignments/APS98-A0/APS98-A0_02.cpp
@@ -5,62 +5,61 @@ using namespace std;
 int ch_to_digit(char ch);
 bool is_charkheshi(string num_str);
 bool is_all_seen_once(vector<bool> is_seen);
-void print_result(bool result);
+void print_result(bool result, int counter);
 
 int main()
 {
-  string num_str;
-  cin >> num_str;
+    for (int counter = 1;; counter++)
+    {
+        string num_str;
+        cin >> num_str;
+        if (num_str == "0")
+            break;
 
-  int counter = 1;
-  while (num_str != "0")
-  {
-    cout << "Case " << counter << ": ";
-    print_result(is_charkheshi(num_str));
-    cin >> num_str;
-    counter++;
-  }
-  return 0;
+        print_result(is_charkheshi(num_str), counter);
+    }
+    return 0;
 }
 
 int ch_to_digit(char ch)
 {
-  return (int)ch - 48;
+    return (int)ch - 48;
 }
 
 bool is_charkheshi(string num_str)
 {
-  int size = num_str.size();
-  vector<bool> is_seen(size, false);
-  int next_digit_pos = 0;
-  while (true)
-  {
-    int digit = ch_to_digit(num_str[next_digit_pos]);
-    next_digit_pos = (next_digit_pos + digit) % size;
-    if (is_seen[next_digit_pos])
-      break;
-    else
-      is_seen[next_digit_pos] = true;
-  }
+    int size = num_str.size();
+    vector<bool> is_seen(size, false);
+    int next_digit_pos = 0;
+    while (true)
+    {
+        int digit = ch_to_digit(num_str[next_digit_pos]);
+        next_digit_pos = (next_digit_pos + digit) % size;
+        if (is_seen[next_digit_pos])
+            break;
+        else
+            is_seen[next_digit_pos] = true;
+    }
 
-  return is_all_seen_once(is_seen);
+    return is_all_seen_once(is_seen);
 }
 
 bool is_all_seen_once(vector<bool> is_seen)
 {
-  int size = is_seen.size();
-  for (int state = 0; state < size; state++)
-  {
-    if (!is_seen[state])
-      return false;
-  }
-  return true;
+    int size = is_seen.size();
+    for (int state = 0; state < size; state++)
+    {
+        if (!is_seen[state])
+            return false;
+    }
+    return true;
 }
 
-void print_result(bool result)
+void print_result(bool result, int counter)
 {
-  if (result)
-    cout << "Yes" << endl;
-  else
-    cout << "No" << endl;
+    cout << "Case " << counter << ": ";
+    if (result)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
 }
